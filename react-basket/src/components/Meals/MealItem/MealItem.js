8013204ea12948dc6881a8c117f77b880ec5fbca
gksrud316 <gksrud316@naver.com>
2023-01-17 17:12:43 +0900

    mid commit

--- react-basket/src/components/Meals/MealItem/MealItem.js
+++ react-basket/src/components/Meals/MealItem/MealItem.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import classes from "./MealItem.module.css"
+import MealItemForm from './MealItemForm';
 const MealItem = (props) => {
     const price= `$${props.price.toFixed(2)}`;
-    return (
+    return ( 
         <li className={classes.meal}> 
             <div>
             <h3>{props.name}</h3>
@@ -10,7 +11,7 @@ const MealItem = (props) => {
             <div className={classes.price}>{price}</div>
             </div>
             <div>
-
+            <MealItemForm/>
             </div>
         </li>
     );
