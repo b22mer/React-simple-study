c8f882fb4d256f0a3059d9c85bac6a9ba63fbd3d
정원철 <gksrud316@naver.com>
2023-01-27 00:55:07 +0900

    http: fetch 비동기 처리와 isLoading을 이용한 로딩처리

--- react-http/src/App.js
+++ react-http/src/App.js
@@ -5,12 +5,31 @@ import './App.css';
 
 function App() {
   const [movies, setMovies]= useState([])
-  const fetchMoviesHandler = ()=>{
-    fetch('https://swapi.dev/api/films/')
-    .then((res)=>{
-      return res.json()
-    }).then((data)=>{
-      const transformedMovies = data.results.map(movieData=>{
+  const [isLoading, setIstLoading]= useState(false);
+  //동기화 코드
+  // const fetchMoviesHandler = ()=>{
+  //   fetch('https://swapi.dev/api/films/')
+  //   .then((res)=>{
+  //     return res.json()
+  //   }).then((data)=>{
+  //     const transformedMovies = data.results.map(movieData=>{
+  //       return{
+  //         id: movieData.episode_id,
+  //         title: movieData.title,
+  //         openingText: movieData.opening_crawl,
+  //         releaseDate: movieData.release_date
+  //       }
+  //     })
+  //     setMovies(transformedMovies)
+  //   })
+  // }
+
+  //비동기화 코드
+  const fetchMoviesHandler =  async ()=>{
+    setIstLoading(true);
+    const res=await fetch('https://swapi.dev/api/films/');
+    const data= await res.json();
+        const transformedMovies = data.results.map(movieData=>{
         return{
           id: movieData.episode_id,
           title: movieData.title,
@@ -19,7 +38,7 @@ function App() {
         }
       })
       setMovies(transformedMovies)
-    })
+      setIstLoading(false);
   }
 
   return (
@@ -28,7 +47,10 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {~isLoading && movies.length>0 &&<MoviesList movies={movies} />}
+        {!isLoading && movies.length===0 && <p>Found no movies</p>}
+        {isLoading && <p>Loading...</p>}
+        
       </section>
     </React.Fragment>
   );
